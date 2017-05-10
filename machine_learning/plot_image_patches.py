d7184f4e9507d30385e797dc6c035b08630d45d9
Xiangchun Li <364592279@qq.com>
2017-05-11 00:14:04 +0800

    update extract image example

--- machine_learning/plot_image_patches.py
+++ machine_learning/plot_image_patches.py
@@ -1,9 +1,9 @@
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 
-def plot(samples, ksize_rows=299, ksize_cols=299):
-    nr = samples.shape[1]
-    nc = samples.shape[2]
+def plot(x, ksize_rows=299, ksize_cols=299):
+    nr = x.shape[1]
+    nc = x.shape[2]
     fig = plt.figure(figsize=(nr, nc))
     gs = gridspec.GridSpec(nr, nc)
     gs.update(wspace=0.05, hspace=0.05)
@@ -14,12 +14,12 @@ def plot(samples, ksize_rows=299, ksize_cols=299):
             plt.axis('off')
             ax.set_xticklabels([])
             ax.set_yticklabels([])
-            ax.set_aspect('equal')
+            #ax.set_aspect('equal')
             #plt.imshow(sample.reshape(ksize_rows, ksize_cols), cmap='Greys_r')
-            plt.imshow(samples[0,i,j,].reshape(ksize_rows, ksize_cols, 3))
+            plt.imshow(x[0,i,j,].reshape(ksize_rows, ksize_cols, 3))
 
     return fig
 
-#fig = plot(samples)
+#fig = plot(x)
 #plt.savefig('out/{}.png'.format(str(i).zfill(3)), bbox_inches='tight')
 #plt.close(fig)
